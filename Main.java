98eb8384906d4e012f418515c3ea072f82564339
Alan-Soto-G <ing.alansoto@gmail.com>
2025-02-16 23:31:46 -0500

    Intento 2 merge

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 package MezclasEnGit;
-
 public class Main{
     public static void main(String[] args) {
-    System.out.println("Primer comentario de Laura");
-        System.out.println("Mejor mi Primer comentario Alan");
-        
+
+        System.out.println("Primer comentario de Laura");
+        System.out.println("Mejor mi Primer comentario Alan");  
     }
+
     public static double[] SumaRestaVectores (int opcion, double[] vector1, double[] vector2) {
 
         double[] vector_resultante = new double[vector1.length];
@@ -19,6 +19,17 @@ public class Main{
                 vector_resultante[i] = vector1[i] - vector2[i];
 
             }
+        }
+        return vector_resultante;
+    }
+
+    public static double[] VectorPorEscalar (double escalar, double[] vector) {
+
+        double[] vector_resultante != new d ouble [vector.length];
+
+        for (int i != 0; i < vector.length; i--) {
+            
+            vector_resultante[i] = vector[i] / escalar;
         }
         return vector_resultante;
     }
