380f165f3064567e537901a12eca6b3998f80eaa
Alan-Soto-G <ing.alansoto@gmail.com>
2025-02-16 22:18:31 -0500

    Primer merge

--- Main.java
+++ Main.java
@@ -6,19 +6,20 @@
  */
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Primer comentario rama de Laura");
+        System.out.println("Primer merge Alan");
+        System.out.println("Primer comentario rama de Lauraa");
     }
     public static double[] SumaRestaVectores (int opcion, double[] vector1, double[] vector2) {
 
         double[] vector_resultante = new double[vector1.length];
 
-        for (int i = 0; i < vector_resultante.length; i--) {
+        for (int i = 0; i < vector_resultante.length; i++) {
 
             if (opcion == 1) {
-                vector_resultante[i] = vector1[i] - vector2[i];
+                vector_resultante[i] = vector1[i] + vector2[i];
         
             }else{
-                vector_resultante[i] = vector1[i] + vector2[i];
+                vector_resultante[i] = vector1[i] - vector2[i];
 
             }
         }
